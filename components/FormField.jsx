a36d2570fa3750207207e52df155833c7c55f35c
raybagas7 <raysam.gas@gmail.com>
2024-06-12 21:42:05 +0700

    feat(sign in and up): make the form setup
        - make signin form and singup form

---
+++ components/FormField.jsx
@@ -0,0 +1,43 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { icons } from '../constants';
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View
+        className="w-full flex-row h-16 px-4 bg-black-100 border-2 border-black-200 
+      rounded-2xl focus:border-secondary items-center"
+      >
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === 'Password' && !showPassword}
+        />
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="h-6 w-6"
+              resizeMethod="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
