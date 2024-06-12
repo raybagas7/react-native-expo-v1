c4398896de88db2e85553e4d4cab40e806c4c7b9
raybagas7 <raysam.gas@gmail.com>
2024-06-12 18:56:18 +0700

    feat(route): create app routes
        - create auth group with it's own _layout
        - create auth tabs with it's own _layout
        - create auth search with dinamyc route just like nextjs

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SigIn = () => {
+  return (
+    <View>
+      <Text>SigIn</Text>
+    </View>
+  );
+};
+
+export default SigIn;
